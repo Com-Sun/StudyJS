e03ea01b81d0990faf4556a20bfc7d4927b60723
Com-Sun <gnlslfflffl@gmail.com>
2021-12-06 14:41:14 +0900

    add level3 with css

--- WebContent/level3/script.js
+++ WebContent/level3/script.js
@@ -2,16 +2,35 @@ numbers = {};
 numbers.all = [];
 numbers.prime = [];
 
-numbers.isPrime = function (number) {
-
+numbers.addAll = function (max_num) {
+    for (let i = 1; i <= max_num; i++) {
+        this.all.push(i);
+    }
 };
-numbers.addPrime = function (number) {
-
+numbers.isPrime = function (number) {
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+};
+numbers.addPrime = function () {
+    for (let i = 0; i < this.all.length; i++) {
+        if (this.isPrime(i)) {
+            this.prime.push(i);
+        }
+    }
 };
 numbers.getAllPrimes = function () {
-
+    let strPrime = "";
+    strPrime += "<h2> 전체 소수의 개수: " + this.prime.length + "</h2>"
+    for (let i = 2; i < this.prime.length; i ++) {
+        strPrime += this.prime[i] + "<br>";
+    }
+    return strPrime;
 };
 
-numbers.addAll = function (max_num) {
-
-};
+numbers.addAll(10000);
+numbers.addPrime();
+document.getElementById("result").innerHTML = numbers.getAllPrimes();
